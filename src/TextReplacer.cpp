3e5675cd860789513c2c9ba70187b919cab99353
ixpl0 <telegram@ixplo>
2024-03-07 00:52:51 +0400

    restore clipboard after layout change

--- src/TextReplacer.cpp
+++ src/TextReplacer.cpp
@@ -8,41 +8,76 @@
 std::wstring transformText(const std::wstring& originalText);
 
 void TextReplacer::replaceSelectedText() {
-    keybd_event(VK_CONTROL, 0, 0, 0);
-    keybd_event('C', 0, 0, 0);
-    keybd_event('C', 0, KEYEVENTF_KEYUP, 0);
-    keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
-    Sleep(50);
-
     if (OpenClipboard(nullptr)) {
-        HANDLE hClipboardData = GetClipboardData(CF_UNICODETEXT);
-        if (hClipboardData) {
-            wchar_t* pchData = static_cast<wchar_t*>(GlobalLock(hClipboardData));
-            if (pchData) {
-                std::wstring originalText(pchData);
-                GlobalUnlock(hClipboardData);
-
-                std::wstring transformedText = transformText(originalText);
-
-                EmptyClipboard();
-                HGLOBAL hNewClipboardData = GlobalAlloc(GMEM_DDESHARE, static_cast<DWORD>((transformedText.size() + 1) * sizeof(wchar_t)));
-                if (hNewClipboardData) {
-                    wchar_t* pNewClipboardData = static_cast<wchar_t*>(GlobalLock(hNewClipboardData));
-                    if (pNewClipboardData) {
-                        wcscpy_s(pNewClipboardData, transformedText.size() + 1, transformedText.c_str());
-                        GlobalUnlock(hNewClipboardData);
-                        SetClipboardData(CF_UNICODETEXT, hNewClipboardData);
-                    }
-                }
+        // save initial clipboard
+        HANDLE hOriginalClipboardData = GetClipboardData(CF_UNICODETEXT);
+        std::wstring originalClipboardText;
+        if (hOriginalClipboardData) {
+            wchar_t* pchOriginalData = static_cast<wchar_t*>(GlobalLock(hOriginalClipboardData));
+            if (pchOriginalData) {
+                originalClipboardText = pchOriginalData;
+                GlobalUnlock(hOriginalClipboardData);
             }
         }
         CloseClipboard();
-    }
 
-    keybd_event(VK_CONTROL, 0, 0, 0);
-    keybd_event('V', 0, 0, 0);
-    keybd_event('V', 0, KEYEVENTF_KEYUP, 0);
-    keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+        // copy text
+        keybd_event(VK_CONTROL, 0, 0, 0);
+        keybd_event('C', 0, 0, 0);
+        keybd_event('C', 0, KEYEVENTF_KEYUP, 0);
+        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+        Sleep(50);
+
+        if (OpenClipboard(nullptr)) {
+            // get text from clipboard
+            HANDLE hClipboardData = GetClipboardData(CF_UNICODETEXT);
+            if (hClipboardData) {
+                wchar_t* pchData = static_cast<wchar_t*>(GlobalLock(hClipboardData));
+                if (pchData) {
+                    std::wstring originalText(pchData);
+                    GlobalUnlock(hClipboardData);
+
+                    // change text layout
+                    std::wstring transformedText = transformText(originalText);
+
+                    EmptyClipboard();
+                    // set converted text to clipboard
+                    HGLOBAL hNewClipboardData = GlobalAlloc(GMEM_DDESHARE, static_cast<DWORD>((transformedText.size() + 1) * sizeof(wchar_t)));
+                    if (hNewClipboardData) {
+                        wchar_t* pNewClipboardData = static_cast<wchar_t*>(GlobalLock(hNewClipboardData));
+                        if (pNewClipboardData) {
+                            wcscpy_s(pNewClipboardData, transformedText.size() + 1, transformedText.c_str());
+                            GlobalUnlock(hNewClipboardData);
+                            SetClipboardData(CF_UNICODETEXT, hNewClipboardData);
+                        }
+                    }
+                }
+            }
+            CloseClipboard();
+        }
+
+        // paste converted text
+        keybd_event(VK_CONTROL, 0, 0, 0);
+        keybd_event('V', 0, 0, 0);
+        keybd_event('V', 0, KEYEVENTF_KEYUP, 0);
+        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+        Sleep(50);
+
+        // restore initial clipboard
+        if (OpenClipboard(nullptr)) {
+            EmptyClipboard();
+            HGLOBAL hNewOriginalClipboardData = GlobalAlloc(GMEM_MOVEABLE, (originalClipboardText.size() + 1) * sizeof(wchar_t));
+            if (hNewOriginalClipboardData) {
+                wchar_t* pNewOriginalClipboardData = static_cast<wchar_t*>(GlobalLock(hNewOriginalClipboardData));
+                if (pNewOriginalClipboardData) {
+                    wcscpy_s(pNewOriginalClipboardData, originalClipboardText.size() + 1, originalClipboardText.c_str());
+                    GlobalUnlock(hNewOriginalClipboardData);
+                    SetClipboardData(CF_UNICODETEXT, hNewOriginalClipboardData);
+                }
+            }
+            CloseClipboard();
+        }
+    }
 }
 
 bool isMostlyFirstLanguage(const std::wstring& originalText, const std::wstring& chars1, const std::wstring& chars2) {
